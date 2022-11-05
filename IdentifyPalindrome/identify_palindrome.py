3efbe9982b81594e4e6dad2cf8c06ddc5619d46c
Aman Pandya <aman15.pandya@gmail.com>
2022-11-05 09:58:27 +0000

    moved IdentifyPalindrome folder to root folder and added tests

---
+++ IdentifyPalindrome/identify_palindrome.py
@@ -0,0 +1,6 @@
+def is_palindrome(string):
+    pass
+
+if __name__ == "__main__":
+    string = input("Enter string (using letters A-Z, a-z): ")
+    is_palindrome(string)
---
+++ IdentifyPalindrome/test.py
@@ -0,0 +1,46 @@
+from identify_palindrome import is_palindrome
+import unittest
+
+class TestIdentifyPalindrome(unittest.TestCase):
+
+    def test_multi_letter_palindrome(self):
+        """
+        Test if a multiple letter palindrome is identified
+        """
+
+        answer = is_palindrome("racecar")
+        self.assertTrue(answer)
+
+    def test_multi_letter_palindrome(self):
+        """
+        Test if a palindrome with single letter is identified
+        """
+
+        answer = is_palindrome("a")
+        self.assertTrue(answer)
+
+    def test_palindrome_case_insensitivity(self):
+        """
+        Test if a palindrome check is case insensitive
+        """
+
+        answer = is_palindrome("Racecar")
+        self.assertTrue(answer)
+
+    def test_false_palindrome_check(self):
+        """
+        Test if a false palindrome is identified
+        """
+        answer = is_palindrome("Toy")
+        self.assertFalse(answer)
+
+    def test_invalid_character_exception(self):
+        """
+        Test if on entering invalid character, the function throws exception or not
+        """
+        
+        with self.assertRaises(Exception):
+            answer = is_palindrome("Toy")
+        
+if __name__ == "__main__":
+    unittest.main()
