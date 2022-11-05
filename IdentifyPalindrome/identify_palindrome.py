b0d4b7da91a54d5f70f7dc4123821384317bbcf4
Aman Pandya <aman15.pandya@gmail.com>
2022-11-05 10:19:30 +0000

    added logic to identify palindrome and tested it

--- IdentifyPalindrome/identify_palindrome.py
+++ IdentifyPalindrome/identify_palindrome.py
@@ -1,6 +1,23 @@
 def is_palindrome(string):
-    pass
+    if not string.isalpha():
+        raise Exception("Invalid String entered. Use A-z, a-z")
+
+    lowercase_string = string.lower()
+
+    start = 0
+    end = len(lowercase_string) - 1
+    while start < end:
+        if(lowercase_string[start] != lowercase_string[end]):
+            return False
+        start += 1
+        end -= 1
+
+    return True
 
 if __name__ == "__main__":
-    string = input("Enter string (using letters A-Z, a-z): ")
-    is_palindrome(string)
+    string = input("Enter string (A-z, a-z): ")
+    answer = is_palindrome(string)
+    if answer:
+        print(f"The string {string} is palindrome")
+    else:
+        print(f"The string {string} is not a palindrome")
