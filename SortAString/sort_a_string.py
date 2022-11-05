de4769e4f7565262bd5804fdce069089a1dd3cc4
Aman Pandya <aman15.pandya@gmail.com>
2022-11-05 10:40:26 +0000

    added dummy function to sort_a_string.py

---
+++ SortAString/sort_a_string.py
@@ -0,0 +1,7 @@
+def sort_string(string):
+    pass
+
+if __name__ == "__main__":
+    string = input("Enter string of words: ")
+    answer = sort_string(string)
+    print(f"The sorted string is: {answer}")
