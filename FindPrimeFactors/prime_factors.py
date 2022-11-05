4e33876768130bd5619f4204c047ccc3bab2c40a
Aman Pandya <aman15.pandya@gmail.com>
2022-11-05 08:21:26 +0000

    Solved Prime Factor Challenge

---
+++ FindPrimeFactors/prime_factors.py
@@ -0,0 +1,16 @@
+def find_prime_factors(number):
+    prime_factors = []
+    divisor = 2
+    while number > 1:
+        if number % divisor == 0:
+            prime_factors.append(divisor)
+            number = number // divisor
+        else:
+            divisor += 1
+
+    return prime_factors
+
+if __name__ == "__main__":
+    number = int(input("Enter number: "))
+    prime_factors = find_prime_factors(number)
+    print(f"The prime factors for {number} are {prime_factors}")
