30d0f47ed5a971b7556720c61cc800a9e64d10fd
Aman Pandya <aman15.pandya@gmail.com>
2022-11-05 16:48:16 +0000

    changed test case logic to use a floating timetaken field instead of a 3 decimal place variable

--- WaitingGame/test.py
+++ WaitingGame/test.py
@@ -6,17 +6,20 @@ class TestWaitingGame(unittest.TestCase):
     def test_too_slow(self):
         expected_waiting_time = 4
         actual_waiting_time = 4.213
+        time_taken = actual_waiting_time - expected_waiting_time
         answer = waiting_game(expected_waiting_time, actual_waiting_time)
-        self.assertEqual(answer, WaitingGameResponse(0.213, "too slow"))
+        self.assertEqual(answer, WaitingGameResponse(time_taken, 4.213, "too slow"))
 
     def test_exact(self):
         expected_waiting_time = 4
-        actual_waiting_time = 4.213
+        actual_waiting_time = 4
+        time_taken = actual_waiting_time - expected_waiting_time
         answer = waiting_game(expected_waiting_time, actual_waiting_time)
-        self.assertEqual(answer, WaitingGameResponse(0, "exact"))
+        self.assertEqual(answer, WaitingGameResponse(time_taken, 4, "exact"))
 
     def test_too_fast(self):
         expected_waiting_time = 4
         actual_waiting_time = 3.213
+        time_taken = actual_waiting_time - expected_waiting_time
         answer = waiting_game(expected_waiting_time, actual_waiting_time)
-        self.assertEqual(answer, WaitingGameResponse(-0.787, "too fast"))
+        self.assertEqual(answer, WaitingGameResponse(time_taken, 3.213, "too fast"))
