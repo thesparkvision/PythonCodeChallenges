e6ea5f65ecb335bb29e09a8f7edc39395869e959
Aman Pandya <aman15.pandya@gmail.com>
2022-11-06 10:33:07 +0000

    used common class for return response of both operations

--- SaveDictionary/save_dictionary.py
+++ SaveDictionary/save_dictionary.py
@@ -1,39 +1,35 @@
 from dataclasses import dataclass
 
 @dataclass
-class LoadResponse:
-    loaded_dictionary: dict
-    load_status: str
-    load_error: str
+class OperationResponse:
+    data: object
+    status: str
+    error: str
 
-@dataclass
-class SaveResponse:
-    save_message: str
-
-def load_dictionary(file_path: str) -> LoadResponse:
+def load_dictionary(file_path: str) -> OperationResponse:
     pass
 
-def save_dictionary(dictionary: dict, file_path: str) -> SaveResponse:
+def save_dictionary(dictionary: dict, file_path: str) -> OperationResponse:
     pass
 
 def run_dictionary_save_interactions():
     user_input = eval(input())
     file_path = input()
-    
+
     if type(user_input) != "dict":
         print("Input is not a valid dictionary!")
 
     save_response = save_dictionary(dictionary, file_path)
-    print(f"Dictionary Save Status: {save_response}")
+    print(f"Dictionary Save Status: {save_response.status}")
 
-    if save_response == "Failure":
+    if save_response.status == "Failure":
         return
 
     load_response = load_dictionary(file_path)
-    if load_response.load_status == "Success":
-        print(f"Loaded Dictionary: {load_response.loaded_dictionary}")
+    if load_response.status == "Success":
+        print(f"Loaded Dictionary: {load_response.data}")
     else:
-        print(f"Load operation failure: {load_response.load_error}")
+        print(f"Load operation failure: {load_response.error}")
 
 if __name__ == "__main__":
     run_dictionary_save_interactions()
