b77231c33dff56eb806fc8556a5bb2147b169356
Aman Pandya <aman15.pandya@gmail.com>
2022-11-07 04:12:31 +0000

    divide save dictionary feature into separate files on basis of objective

---
+++ SaveDictionary/models.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+from enum import Enum
+
+class OperationStatus(Enum):
+    SUCCESS = "Success"
+    FAILURE = "Failure"
+
+@dataclass
+class OperationResponse:
+    status: OperationStatus
+    data: object = None
+    error: str = ""
